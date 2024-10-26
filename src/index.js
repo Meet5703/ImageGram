379c6f531da2b01362b7fcd0105e843d6376a88f
prayagpandya <pandyaprayagraj@gmail.com>
2024-10-26 10:47:03 +0530

    added 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import connectDB from './config/dbConfig.js';
-import apiRouter from './routers/apiRouter.js';
-import multer from 'multer';
-import { isAuthenticated } from './middlewares/authMiddleware.js';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import {options} from './utils/swaggerOptions.js';
+import express from "express";
+import connectDB from "./config/dbConfig.js";
+import apiRouter from "./routers/apiRouter.js";
+import multer from "multer";
+import { isAuthenticated } from "./middlewares/authMiddleware.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { options } from "./utils/swaggerOptions.js";
 
 const PORT = 3000; // port number
 
@@ -16,20 +16,18 @@ app.use(express.text());
 app.use(express.urlencoded());
 
 const swaggerDocs = swaggerJSDoc(options);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use('/api', apiRouter);// If the url starts with /api then the request is forwarded to the apiRouter
+app.use("/api", apiRouter); // If the url starts with /api then the request is forwarded to the apiRouter
 
-
-
-app.get('/ping', isAuthenticated, (req, res) => {
-    console.log(req.query);
-    console.log(req.body);
-    console.log(req.user);
-    return res.json({ message: 'pong' });
+app.get("/ping", (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  console.log(req.user);
+  return res.json({ message: "pong" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
